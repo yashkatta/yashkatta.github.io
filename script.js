8f93a7767d5104bb29d42a6fc89bf6e0f7ca5649
Yash Katta <yash.katta1@gmail.com>
2026-05-27 17:39:09 +0530

    Added the Swiper Script and its supporting code

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => {
     menuOpenButton.click();
 });
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
+});
